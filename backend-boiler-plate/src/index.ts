93957851197876b6133865408bdb424f3a5dedfe
lucaswunder <wundervald_2@hotmail.com>
2022-12-18 23:33:47 -0300

    updated index file to require container file

--- backend-boiler-plate/src/index.ts
+++ backend-boiler-plate/src/index.ts
@@ -3,15 +3,16 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import mongoose from 'mongoose'
 
-import userRoutes from './routes/user'
+import notFoundHandler from './_shared/notFoundHandler'
+import errorHandler from './_shared/errorHandler'
 
 const HOST = process.env.HOST || 'https://localhost'
 const PORT = process.env.PORT || 8000
 const LOGMSG = '⚡️[Paketá Credito Live-Coding BoilerPlate]:'
 
 mongoose.connect(
-    process.env.MONGO_URL || 'mongodb://localhost:27017/local',
-    {},
+    process.env.MONGO_URL || 'mongodb://admin:paketa2023@localhost',
+    { dbName: 'paketa' },
     err => {
         const msg = err
             ? `${LOGMSG} Failed to connect to MongoDB: ${err}`
@@ -20,9 +21,23 @@ mongoose.connect(
     },
 )
 
+import container from './container'
+
+const menuRoutes = container.cradle['routesMenu']
+const userRoutes = container.cradle['routesUser']
+
 const app = express()
+
 app.use(express.json())
+
 app.use('/user', userRoutes)
+
+app.use('/menu', menuRoutes)
+
+app.use('/*', notFoundHandler)
+
+app.use(errorHandler)
+
 app.listen(PORT, () => {
     console.log(`${LOGMSG} Server is running at ${HOST}:${PORT}`)
 })
